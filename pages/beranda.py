46bd33fb29cd7ca8716f766a32c1b6ecfab3b610
Andrian Baros <andrianbaros10@gmail.com>
2026-02-11 21:09:21 +0700

    Update beranda.py

--- pages/beranda.py
+++ pages/beranda.py
@@ -102,7 +102,7 @@ if uploaded_file:
         # ===============================
         model = SARIMAX(
             ts,
-            order=(1, 0, 1),   # AR(1) stabil
+            order=(1, 0, 0),   # AR(1) stabil
             seasonal_order=(1, 0, 1, 7),
             enforce_stationarity=True,
             enforce_invertibility=True
