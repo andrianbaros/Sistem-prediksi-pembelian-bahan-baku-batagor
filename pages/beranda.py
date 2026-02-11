d8c920494c080f1ab54ebc2e1261dcabc63b8c44
Andrian Baros <andrianbaros10@gmail.com>
2026-02-11 21:30:12 +0700

    Update beranda.py

--- pages/beranda.py
+++ pages/beranda.py
@@ -7,9 +7,10 @@ UI.load_style()
 # ===============================
 # PROTEKSI LOGIN
 # ===============================
-if "sudah_login" not in st.session_state or not st.session_state.sudah_login:
-    st.warning("Silakan login terlebih dahulu")
+if "logged_in" not in st.session_state or not st.session_state.logged_in:
+    st.warning("Anda harus login terlebih dahulu untuk mengakses halaman ini.")
     st.stop()
+
 
 autentikasi = AuthManager()
 
